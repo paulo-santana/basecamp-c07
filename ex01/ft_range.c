da1f6f60ba91498b95a9feb66b5d2f9fc777c5d1
psergio- <psergio-@student.42sp.org.br>
2021-04-16 01:32:32 +0000

    norme

--- ex01/ft_range.c
+++ ex01/ft_range.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/16 01:30:58 by psergio-          #+#    #+#             */
+/*   Updated: 2021/04/16 01:31:20 by psergio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max)
 {
 	int	i;
 	int	*range;
@@ -9,11 +21,11 @@ int *ft_range(int min, int max)
 	if (min >= max)
 		return (0);
 	size = max - min;
-	range = (int *) malloc(sizeof(int) * size);
+	range = (int *)malloc(sizeof(int) * size);
 	if (range == 0)
 		return (0);
 	i = -1;
 	while (++i < size)
 		range[i] = min++;
-	return range;
+	return (range);
 }
